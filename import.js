2794791616d622966704958c74cc6102abb6d180
Nikita Sokolov <n.sokolov@agro.club>
2022-04-04 18:55:53 +0300

    ек

--- import.js
+++ import.js
@@ -1,7 +1,41 @@
 "use strict";
 
-const importContainers = $("importContainer");
+const importContainers = $(".importContainer");
 
-importContainers.each(() => {
-  console.log($(this));
+importContainers.each((index) => {
+  const item = importContainers[index];
+
+  const url = item.dataset.htmlStaticUrl;
+
+  if (url) {
+    fetch(url).then((response) => {
+      response.text().then((text) => {
+        console.log(text);
+        $(item).html(text);
+        nodeScriptReplace(item);
+      });
+    });
+  }
 });
+
+const isScript = (node) => node.tagName === "SCRIPT";
+
+const nodeScriptReplace = (node) => {
+  const script = $(node).children("script");
+  script.replaceWith(nodeScriptClone(script));
+};
+
+const nodeScriptClone = (node) => {
+  const script = document.createElement("script");
+  script.text = node.innerHTML;
+
+  let i = -1;
+  let attr;
+
+  const attrs = node.attributes;
+  while (++i < attrs.length) {
+    script.setAttribute((attr = attrs[i]).name, attr.value);
+  }
+
+  return script;
+};
