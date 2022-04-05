9db0dbcaa202fa4510cb29cccda16f3a2a136161
Nikita Sokolov <n.sokolov@agro.club>
2022-04-05 16:37:35 +0300

    refactor

--- redirect/redirect.ts
+++ redirect/redirect.ts
@@ -1,101 +1,103 @@
-const url = location.pathname;
+$(document).ready(() => {
+  const url = location.pathname;
 
-const regions = {
-  us: {
-    title: "USA",
-    root: "/",
-  },
-  ca: {
-    title: "Canada",
-    root: "/ca",
-  },
-  es: {
-    title: "Spain",
-    root: "/es",
-  },
-  ru: {
-    title: "Russia",
-    root: "/ru",
-  },
-};
+  const regions = {
+    us: {
+      title: "USA",
+      root: "/",
+    },
+    ca: {
+      title: "Canada",
+      root: "/ca",
+    },
+    es: {
+      title: "Spain",
+      root: "/es",
+    },
+    ru: {
+      title: "Russia",
+      root: "/ru",
+    },
+  };
 
-const regionKeys = Object.keys(regions);
+  const regionKeys = Object.keys(regions);
 
-//Functions
-const setRegionOverride = (region: string) => {
-  sessionStorage.setItem("OVERRIDE_REGION", region);
-};
+  //Functions
+  const setRegionOverride = (region: string) => {
+    sessionStorage.setItem("OVERRIDE_REGION", region);
+  };
 
-const getRegionOverride = () => sessionStorage.getItem("OVERRIDE_REGION");
+  const getRegionOverride = () => sessionStorage.getItem("OVERRIDE_REGION");
 
-const getRegionByUrl = () => {
-  switch (true) {
-    case url.includes(regions.ca.root):
-      return "ca";
-    case url.includes(regions.es.root):
-      return "es";
-    case url.includes(regions.ru.root):
-      return "ru";
-    default:
-      return "us";
-  }
-};
-
-const redirect = (region: string) => {
-  location.replace(`${location.origin}${regions[region].root}`);
-};
-
-const regionByUrl = getRegionByUrl();
-const regionOverride = getRegionOverride();
-
-const span = $('[data-replace-key="detectedRegion"]');
-
-const onCountryDetect = () => {
-  const detectedRegion = span.text();
-
-  if (regionKeys.includes(detectedRegion)) {
-    // Elements
-    const notificationRoot = $("#regionNotification_root");
-    const notificationCountry = $("#regionNotification_country");
-    const notificationForm = $("#regionNotification_form");
-    const notificationCloseButton = $("#regionNotification_close");
-
-    //Event handlers
-    const onClickClose = () => {
-      notificationRoot.removeClass("regionNotification_root_visible");
-      setRegionOverride(regionByUrl);
-    };
-
-    const onSubmit = (e) => {
-      e.preventDefault();
-      const code = notificationCountry.val();
-      notificationRoot.removeClass("regionNotification_root_visible");
-
-      if (typeof code === "string" && code !== regionByUrl) {
-        setRegionOverride(code);
-        redirect(code);
-      }
-    };
-
-    notificationCloseButton.on("pointerup", onClickClose);
-    notificationForm.on("submit", onSubmit);
-
-    //Main
-
-    const isRegionMatched = detectedRegion === regionByUrl;
-
-    if (!isRegionMatched && !regionOverride) {
-      notificationRoot.addClass("regionNotification_root_visible");
+  const getRegionByUrl = () => {
+    switch (true) {
+      case url.includes(regions.ca.root):
+        return "ca";
+      case url.includes(regions.es.root):
+        return "es";
+      case url.includes(regions.ru.root):
+        return "ru";
+      default:
+        return "us";
     }
-    notificationCountry.html(
-      Object.entries(regions).reduce((acc, [key, region]) => {
-        acc += `<option value="${key}" ${
-          detectedRegion === key ? 'selected="true"' : ""
-        }>${region.title}</option>`;
-        return acc;
-      }, "")
-    );
-  }
-};
+  };
 
-span.on("DOMSubtreeModified", onCountryDetect);
+  const redirect = (region: string) => {
+    location.replace(`${location.origin}${regions[region].root}`);
+  };
+
+  const regionByUrl = getRegionByUrl();
+  const regionOverride = getRegionOverride();
+
+  const span = $('[data-replace-key="detectedRegion"]');
+
+  const onCountryDetect = () => {
+    const detectedRegion = span.text();
+
+    if (regionKeys.includes(detectedRegion)) {
+      // Elements
+      const notificationRoot = $("#regionNotification_root");
+      const notificationCountry = $("#regionNotification_country");
+      const notificationForm = $("#regionNotification_form");
+      const notificationCloseButton = $("#regionNotification_close");
+
+      //Event handlers
+      const onClickClose = () => {
+        notificationRoot.removeClass("regionNotification_root_visible");
+        setRegionOverride(regionByUrl);
+      };
+
+      const onSubmit = (e) => {
+        e.preventDefault();
+        const code = notificationCountry.val();
+        notificationRoot.removeClass("regionNotification_root_visible");
+
+        if (typeof code === "string" && code !== regionByUrl) {
+          setRegionOverride(code);
+          redirect(code);
+        }
+      };
+
+      notificationCloseButton.on("pointerup", onClickClose);
+      notificationForm.on("submit", onSubmit);
+
+      //Main
+
+      const isRegionMatched = detectedRegion === regionByUrl;
+
+      if (!isRegionMatched && !regionOverride) {
+        notificationRoot.addClass("regionNotification_root_visible");
+      }
+      notificationCountry.html(
+        Object.entries(regions).reduce((acc, [key, region]) => {
+          acc += `<option value="${key}" ${
+            detectedRegion === key ? 'selected="true"' : ""
+          }>${region.title}</option>`;
+          return acc;
+        }, "")
+      );
+    }
+  };
+
+  span.on("DOMSubtreeModified", onCountryDetect);
+});
